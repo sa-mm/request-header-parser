b71118554845eaf81a341b7c22680303141c0c78
blah blah <sa-mm@users.noreply.github.com>
2017-02-17 21:34:14 -0600

    Add functionality

--- index.js
+++ index.js
@@ -4,7 +4,19 @@ var app = express();
 app.set('port', (process.env.PORT || 3000));
 
 app.get('/', function(req, res) {
-    res.send("HI");
+    var ip = req.ip;
+    var lang = req.acceptsLanguages()[0];
+    var userAgentString = req.headers['user-agent'];
+    var re = /(\(.*?\))/;
+    var operatingSystem= userAgentString.match(re)[0].replace(/[\(\)]/g,'');
+    var response = {
+        "ipaddress": ip,
+        "language": lang,
+        "software": operatingSystem
+    }
+    console.log(response);
+
+    res.send(response);
 });
 
 app.listen(app.get('port'), function() {
